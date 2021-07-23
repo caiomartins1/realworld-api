52f731f091255bd7d4670c17721d036b91034480
Caio Martins <dev.caiomartins@gmail.com>
2021-07-23 19:41:44 +0100

    :white_check_mark: Added User entity

---
+++ src/entities/User.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password_hash: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
