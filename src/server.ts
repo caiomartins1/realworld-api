c50c2b7be833379c9432d30bf127bd2c0f3f02e2
Caio Martins <dev.caiomartins@gmail.com>
2021-07-23 19:34:54 +0100

    :+1: removed sample route and added router to server

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
 import 'reflect-metadata';
 import './database';
+
 import express from 'express';
 import cors from 'cors';
+
+import { router } from './routes';
 
 const app = express();
 const port = process.env.SERVER_PORT;
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/', async (req, res) => {
-  console.log('hello');
-  return res.json('Hello world');
-});
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server listenning at: http://localhost:${port}`);
