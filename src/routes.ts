82a386a739ba9b185e468c93dd8c64e094e18f3c
Caio Martins <dev.caiomartins@gmail.com>
2021-07-24 16:57:38 +0100

    :+1: Added PORT /api/users/login route

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateUserController } from './controllers/CreateUserController';
 
 // Controllers
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
 
 const router = Router();
 
 // Users Routes
 router.post('/api/users', createUserController.handle);
+router.post('/api/users/login', authenticateUserController.handle);
 
 export { router };
